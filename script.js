4fd6016fa8545dc39ed2735c49c1da97ff15ed7d
rf-spuds <rfspuds@gmail.com>
2020-03-16 19:06:20 -0400

    fixed search bug

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ var uCity;
 var key = "0cfb89d7960d5eaeceaeda6dff8c4963";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + uCity + "&appid=" + key;
 
-function getWeather() {
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
-
-
-};
 
 $("#searchBtn").click(function(event) {
     event.preventDefault();
-    uCity = $("#uCitySearch").val();
+    var uCity = $("#uCitySearch").val();
     getWeather();
-    uSearches.push(uCity);
-
-    localStorage.setItem("savedUserSearches", JSON.stringify(uSearches));
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + uCity + "&appid=" + key;
+    // uSearches.push(uCity);
+    // localStorage.setItem("savedUserSearches", JSON.stringify(uSearches));
     console.log("Search Button worked")
+    console.log(uCity);
 });
+
+
+
+function getWeather() {
+    $(document).ready(function() {
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+    });
+
+};
