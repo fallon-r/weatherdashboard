2e23c2cabe967a5f49d7704652dec64a688a92d5
rf-spuds <rfspuds@gmail.com>
2020-06-29 15:55:24 -0400

    Resolved the Lat/lon issue

--- script.js
+++ script.js
@@ -30,16 +30,24 @@ $(document).ready(function() {
 
 
     var uSearches = [];
-    var key = "0cfb89d7960d5eaeceaeda6dff8c4963";
+    var key = "d083e7b9276f47f503fbf06a7f04eb88";
+    var geoKey = "43d73a28304a40129ec3c08709f5fa7b"
     var uCity;
-    var currentQueryURL;
-    var forecastQueryURL;
-    var UVIndexURL;
     var today = (new Date().getMonth() + 1) + "/" + new Date().getDate();
+    var weatherURL;
     var lat;
     var lon;
-    var onecall = "https://api.openweathermap.org/data/2.5/onecall?q=&exclude=hourly,daily,minutely&appid=0cfb89d760d5eaeceaeda6dff8c496
-    ";
+
+    var coordGetURL;
+
+    // CONSTRUCTOR FUNCTION TO GET COORDINATES
+    function Query(city, lat, lon){
+        this.city = city;
+        this.lat= lat;
+        this.lon = lon
+    }
+
+    var uQuery = new Query();
 
 
     //var input = document.getElementById("uCitySearch");
@@ -56,84 +64,98 @@ $(document).ready(function() {
 
         event.preventDefault();
         uCity = $("#uCitySearch").val();
-        currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + uCity + "&appid=" + key + "&units=imperial";
-        forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + uCity + "&appid=" + key + "&units=imperial";
-        $("#currentWeatherCard").show()
-        UVIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + key;
+        coordGetURL = "https://api.opencagedata.com/geocode/v1/geojson?q=" + uCity + "&key=" + geoKey + "&no_record=1"
 
-        getWeather();
-        getForecast();
-        getUV();
+        getCoords();
+
+        $("#currentWeatherCard").show()
+
+
+        // getWeather();
+        // getForecast();
         $("#currentDate").text(today);
         // Pushes searches to array
-        uSearches.push(uCity);
-        localStorage.setItem("uSearches", uSearches);
+        // uSearches.push(uCity);
+        // localStorage.setItem("uSearches", uSearches);
 
     });
 
-    function getWeather() {
-        $(document).ready(function() {
-            console.log(currentQueryURL);
-            $.ajax({
-                url: currentQueryURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log("Just today")
-                console.log(response);
-                $("#currentCity").text(response.name)
-                $("#currentTemp").text((parseInt(((response.main.temp))) + "°F"));
+    function getCoords(){
 
-                // - 273.15) * 1.8) + 32)
-                $("#currentConditions").text(response.weather[0].main)
-                $("#currentWindSpeed").text("Wind Speed:" + (parseInt(response.wind.speed * 2.23694)) + " mph ");
-                $("#currentHumidity").text("Humidity " + response.main.humidity + "%");
-
-
-
-
-                // UV is a separate call and needs the lat/lon
-                lon = response.coord.lon;
-                lat = response.coord.lat;
-
-
-            });
-        });
-
-    };
-
-    function getUV() {
-
-        console.log(UVIndexURL)
         $.ajax({
-            url: UVIndexURL,
+            url: coordGetURL,
             method: "GET"
-        }).then(function(response) {
-            console.log("")
-            console.log(response);
+        }).then(function(response){
+            uQuery.city = $("#uCitySearch").val();
+            uQuery.lon = response.features[0].geometry.coordinates[0];
+            uQuery.lat = response.features[0].geometry.coordinates[1];
 
+            lat = uQuery.lat.toString();
+
+            lon = uQuery.lon.toString();
+
+            weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=" + key;
+            uSearches.push("uSearches", JSON.stringify(uQuery));
+
+
+            console.log(uQuery);
+            console.log(weatherURL);
         })
-    };
 
-    function getForecast() {
-        $(document).ready(function() {
-            console.log(forecastQueryURL);
-            $.ajax({
-                url: forecastQueryURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log("Forecast");
-                console.log(response);
 
-                console.log(response.list[0].dt_txt.substr(5, 7))
-                    // Render dates
-                $("#weatherCardDay1").text(response.list[8].dt_txt.substr(5, 6))
-                $("#weatherCardDay2").text(response.list[16].dt_txt.substr(5, 6))
-                $("#weatherCardDay3").text(response.list[24].dt_txt.substr(5, 6))
-                $("#weatherCardDay4").text(response.list[32].dt_txt.substr(5, 6))
-                $("#weatherCardDay5").text(response.list[40].dt_txt.substr(5, 6))
-            });
-        });
-    };
+        }
+
+
+    // function getWeather() {
+    //     $(document).ready(function() {
+    //         console.log(weatherURL);
+    //         $.ajax({
+    //             url: weatherURL,
+    //             method: "GET"
+    //         }).then(function(response) {
+    //             // console.log("Just today")
+    //             console.log("this is the API Call for oneWeather")
+    //             console.log(response);
+
+
+    //         });
+    //     });
+
+    // };
+
+    // function getUV() {
+
+    //     console.log(UVIndexURL)
+    //     $.ajax({
+    //         url: UVIndexURL,
+    //         method: "GET"
+    //     }).then(function(response) {
+    //         console.log("")
+    //         console.log(response);
+
+    //     })
+    // };
+
+    // function getForecast() {
+    //     $(document).ready(function() {
+    //         console.log(forecastQueryURL);
+    //         $.ajax({
+    //             url: forecastQueryURL,
+    //             method: "GET"
+    //         }).then(function(response) {
+    //             console.log("Forecast");
+    //             console.log(response);
+
+    //             console.log(response.list[0].dt_txt.substr(5, 7))
+    //                 // Render dates
+    //             $("#weatherCardDay1").text(response.list[8].dt_txt.substr(5, 6))
+    //             $("#weatherCardDay2").text(response.list[16].dt_txt.substr(5, 6))
+    //             $("#weatherCardDay3").text(response.list[24].dt_txt.substr(5, 6))
+    //             $("#weatherCardDay4").text(response.list[32].dt_txt.substr(5, 6))
+    //             $("#weatherCardDay5").text(response.list[40].dt_txt.substr(5, 6))
+    //         });
+    //     });
+    // };
 
     $("#currentWeatherCard").hide()
 
