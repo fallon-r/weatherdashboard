6e59c627c502c7d1233fa18cd98f2d29a52c5332
rf-spuds <rfspuds@gmail.com>
2020-11-11 22:55:38 -0500

    Cleaned up app dummy page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,68 +10,32 @@ export default function App() {
   const [lon, setLon] = useState(null);
 
   // TODO: Make Weather Call asynchronous; right now the lat and lon take too much time to be returned. I could have the state/town populate the field so that the user can verify that the town is correct. OR use the state from the button click.
-  
-  const handleClick = async (e) => {
+
+  const handleClick = (e) => {
     e.preventDefault();
 
-    if ("geolocation" in navigator) {
-      await navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          // * Lat lon from geolocation
-          setLon(pos.coords.longitude)
-          setLat(pos.coords.latitude)
-
-          const weatherUrl= `/weather/${lat}/${lon}`
-          console.log(weatherUrl)
-          axios
-            .get(weatherUrl, {json:true})
-            .then(res=>{
-                console.log(res.data)
-                const temp= res.data.current.temp
-                setTemp(temp)
-            })
-            .catch(e => {
-              console.error('Something went wrong with the weather server call', e)
-            })
-
-          // *Resolve City based on geolocation
-          const latlon = encodeURIComponent(
-            [pos.coords.latitude, pos.coords.longitude].join()
-          );
-          
-          const reverseUrl = `/reverse/coords/${latlon}`;
-          console.log(reverseUrl);
-          axios
-            .get(reverseUrl, { json: true })
-            .then((res) => {
-              const city = res.data.features[0].properties.components.city;
-              const state = res.data.features[0].properties.components.state;
-
-              setLocation(`${city}, ${state}`);
-
-              const photoUrl= `/photo_search/${state}`
-              axios
-                .get(photoUrl, {json:true})
-                .then(res=>{
-                    // console.log(res.data)
-                    const img= res.data.results[0].urls.full
-                    setPhotoSrc(img)
-                })
-                .catch(e => {
-                  console.error('Something went wrong with the photo server call', e)
-                })
-            })
-            .catch((e) => {
-              console.error("Something went wrong with the server call", e);
-            });
-        },
-        (e) => {
-          console.error("User does not consent to location");
-        }
-      );
-    } else {
-      console.err("I cannot find you");
+    const getCurrentPos = (options = {})=>{
+      return new Promise((resolve, reject)=>{
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+      })
     }
+
+    const loadPos= async () =>{
+      try {
+          const position = await getCurrentPos()
+          const {latitude, longitude} = position.coords
+
+          await setLat(latitude)
+         await  setLon(longitude)
+
+          await console.log(`${lat}; ${lon}`)
+
+      } catch (error) {
+        console.error('UhOH!,', error)
+      }
+    }
+
+    loadPos()
   };
 
   return (
@@ -80,9 +44,16 @@ export default function App() {
       <button onClick={handleClick}>I am a button</button>
       <p> You are here based on your browser location:</p>
       <p>{location}</p>
-      <div style={{minHeight: '40vh', minWidth:'40vw', backgroundSize:'fill', backgroundImage:`url(${photoSrc})`, backgroundSize:'contain'}}>
-      </div>
-  <p> It is currently {temp} &deg;</p>
+      <div
+        style={{
+          minHeight: "40vh",
+          minWidth: "40vw",
+          backgroundSize: "fill",
+          backgroundImage: `url(${photoSrc})`,
+          backgroundSize: "contain",
+        }}
+      ></div>
+      <p> It is currently {temp} &deg;</p>
     </div>
   );
 }
