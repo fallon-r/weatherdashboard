0cc369e9e04ef99dc5ca58ff253f67b33997cd7c
rf-spuds <rfspuds@gmail.com>
2020-11-12 23:06:23 -0500

    material ui dependencies; dashboard base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,95 @@
 import React, { useState, useEffect } from "react";
-import {loadPos} from './utils/utils'
-import {resolveLocation} from './utils/api'
+import { loadPos } from "./utils/utils";
+import { resolveLocation } from "./utils/api";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  makeStyles,
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles/";
+import Nav from './components/Nav'
+
+let theme = createMuiTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#81d4fa",
+    },
+    secondary: {
+      main: "#006064",
+    },
+    background: {
+      default: "#ccc",
+      paper: "#ddd",
+    },
+    divider: "#1a237e",
+  },
+  typography: {
+    fontFamily: "Poppins",
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+}));
+
+theme = responsiveFontSizes(theme);
 
 export default function App() {
+  const { root } = useStyles();
+
   // !States
   // const [location, setLocation] = useState(null);
   // const [photoSrc, setPhotoSrc] = useState(null);
   // const [temp, setTemp] = useState(null);
   const [uCoords, setUCoords] = useState(null);
-  const [button, setButton] = useState(true)
-  
+  const [button, setButton] = useState(true);
 
   // * Get user location on page load
   useEffect(() => {
-    const enableLocation= async ()=>{
-    await loadPos();
-    await setUCoords(JSON.parse(localStorage.getItem("userLocation")))
-  }
-    enableLocation()
+    const enableLocation = async () => {
+      await loadPos();
+      await setUCoords(JSON.parse(localStorage.getItem("userLocation")));
+    };
+    enableLocation();
   }, []);
   // * Button toggle for location based search
-  useEffect(()=>{
-    if(uCoords == null){
-      setButton(true)
-    }else{
-      setButton(false)}
-  }, [uCoords])
-
-
-
-
-  const handleClick =  (e) => {
-    e.preventDefault();
-    const latlon = encodeURIComponent(uCoords.join())
-
-    const getCity =  async () => {
-      const response =  await resolveLocation(latlon)
-      
-      console.log(response)
+  useEffect(() => {
+    if (uCoords == null) {
+      setButton(true);
+    } else {
+      setButton(false);
     }
+  }, [uCoords]);
 
-    getCity()
+  const handleClick = (e) => {
+    e.preventDefault();
+    const latlon = encodeURIComponent(uCoords.join());
 
-    console.log(button)
+    const getCity = async () => {
+      const response = await resolveLocation(latlon);
+
+      console.log(response);
+    };
+
+    getCity();
+
+    console.log(button);
     console.log(uCoords[0]);
-    console.log(uCoords[1])
+    console.log(uCoords[1]);
   };
 
   return (
-    <div>
-      <h1>Hi!</h1>
-      <button onClick={handleClick} disabled={button}>I am a button</button>
-      <p> You are here based on your browser location:</p>
+    <div className="root">
+         <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Nav />
+      </ThemeProvider>
     </div>
   );
 }
